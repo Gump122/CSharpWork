ddb78a49de14d0a9838f492cd5e2b91b8938164c
Gump122 <57785172+Gump122@users.noreply.github.com>
2021-03-12 12:13:17 +0800

    Add files via upload

---
+++ Test01/Test01/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+
+namespace Test01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c = 0;
+            char x;
+            a = int.Parse(Console.ReadLine());
+            x = char.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            switch (x)
+            {
+                case '+': c = a + b; break;
+                case '_': c = a - b; break;
+                case '*': c = a * b; break;
+                case '/':
+                    if (b != 0)
+                    {
+                        c = a / b; break;
+                    }
+                    else
+                    {
+
+                        break;
+                    }
+                case '%':
+                    if (b != 0)
+                    {
+                        c = a % b; break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+            }
+            Console.WriteLine(c);
+        }
+
+    }
+}
+
